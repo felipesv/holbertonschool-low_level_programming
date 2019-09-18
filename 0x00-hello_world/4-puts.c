db68706e77bc59e26ebf301590462a3ee80cf634
Felipe Satizabal <1165@holbertonschool.com>
2019-09-18 18:41:57 +0000

    Fix file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
-#include<stdio.h>
 #include <string.h>
-
-int main()
+/*Show a simple string*/
+int main(void)
 {
-  char varA[50];
-  
-  strcpy(varA, "Programming is like building a multilingual puzzle");
-
-  puts(varA);
-
-  return 0;
-
+char varA[50];
+strcpy(varA, "\"Programming is like building a multilingual puzzle");
+puts(varA);
+return (0);
 }
