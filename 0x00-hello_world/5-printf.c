ce2095a9a30880b4ffd4e2f1022325794929c58d
Felipe Satizabal <1165@holbertonschool.com>
2019-09-18 19:20:43 +0000

    Add file 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Show a simple string using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char varA[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", varA);
+return (0);
+}
