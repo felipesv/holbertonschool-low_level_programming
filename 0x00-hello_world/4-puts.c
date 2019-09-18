b9dad2750a08a7bb438f06d69b3a6e6c603ec222
Felipe Satizabal <1165@holbertonschool.com>
2019-09-18 18:30:08 +0000

    Add file 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <string.h>
+
+int main()
+{
+  char varA[50];
+  
+  strcpy(varA, "Programming is like building a multilingual puzzle");
+
+  puts(varA);
+
+  return 0;
+
+}
