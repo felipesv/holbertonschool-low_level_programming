389aa536dabfd892c3e3245003b6595794797959
Felipe Satizabal <1165@holbertonschool.com>
2019-09-19 20:18:28 +0000

    Fix file 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,23 +6,21 @@
  */
 int main(void)
 {
-  int a;
-  int b;
-  for(a = 48; a <= 57; a++)
-    {
-      for(b = 48; b <= 57; b++)
-	{
-	  putchar(a);
-	  putchar(b);
-	  if(a != 57 || b != 57)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-
-	}
-    }
-  putchar('\n');
-
-  return (0);
+int a;
+int b;
+for (a = 48; a <= 57; a++)
+{
+for (b = 48; b <= 57; b++)
+{
+putchar(a);
+putchar(b);
+if (a != 57 || b != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
