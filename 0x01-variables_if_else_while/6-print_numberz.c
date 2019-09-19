47e6c0910afec5c1b3e785902d6c91a590cfefae
Felipe Satizabal <1165@holbertonschool.com>
2019-09-19 18:26:34 +0000

    Add file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Show the number 0 - 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int start, finish;
+start = 0;
+finish = 9;
+while (start <= finish)
+{
+putchar('0' + start);
+start++;
+}
+putchar('\n');
+return (0);
+}
