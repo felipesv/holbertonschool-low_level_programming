51afc297b31178167bbb9ba65dbf1be972f25842
Felipe Satizabal <1165@holbertonschool.com>
2019-09-24 14:34:07 +0000

    Add file 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,21 @@
 #include "holberton.h"
 /**
- * print_last_digit -  
+ * print_last_digit - print the last digit
  * @n:
  *
  * Return: the absolute number of some number
  */
 int print_last_digit(int n)
 {
-  int lastDigit;
-  char digitPrint, digitReturn;
-  lastDigit = n % 10;
-  digitPrint = '0' + lastDigit;
-  digitReturn = lastDigit;
-  _putchar(digitPrint);
-
-  return (digitReturn);
+int lastDigit;
+char digitPrint, digitReturn;
+lastDigit = n % 10;
+if (lastDigit < 0)
+{
+lastDigit = lastDigit * -1;
+}
+digitPrint = '0' + lastDigit;
+digitReturn = lastDigit;
+_putchar(digitPrint);
+return (digitReturn);
 }
