56dd9d01ec0dd9cfb8e776c6c12ebf076b7ea075
Felipe Satizabal <1165@holbertonschool.com>
2019-09-24 21:46:53 +0000

    Add file 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - display sum of multiples of 3,5
+ *
+ * Return: the absolute number of some number
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for(i = 0; i < 1024; i++)
+	{
+		if((i % 3 == 0) || (i % 5) == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
