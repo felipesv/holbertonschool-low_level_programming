095564de8aa9d2457eb02680731fda0fbc0f1ae5
Felipe Satizabal <1165@holbertonschool.com>
2019-09-25 04:24:00 +0000

    Add file 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - display sum of multiples of 3,5
+ *
+ * Return: the absolute number of some number
+ */
+int main(void)
+{
+	long int i, a = 1, b = 2, c = 0;
+
+	for (i = 0; i < 98; i++)
+	{
+		if (i < 2)
+		{
+			printf("%lu, ", i + 1);
+		}
+		else
+		{
+			c = a + b;
+			a = b;
+			b = c;
+
+			if (i != 97)
+			{
+				printf("%lu, ", c);
+			}
+			else
+			{
+				printf("%lu\n", c);
+			}
+		}
+	}
+
+	return (0);
+}
