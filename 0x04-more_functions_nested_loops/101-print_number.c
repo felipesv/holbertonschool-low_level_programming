6e4f2a564b77c4263b969ccfc61a42a19f5a12b9
Felipe Satizabal <1165@holbertonschool.com>
2019-09-27 04:12:37 +0000

    fix file 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,32 +2,104 @@
 /**
  * print_number - print number
  * @n: number
- *
+ * 
  * Return: is a void
  */
+int numDigit(int number);
+int numRever(int numbDigit, int Number);
+int expo(int counter);
 
 void print_number(int n)
 {
-
-	int numdigit = 0, numN = n,  i, modulo;
-	int a[4];
+	int numdigit, reverNum, lastDigit, i;
 
 	if (n < 0)
 	{
 		n = n * -1;
-		numN = n;
 		_putchar('-');
 	}
 
-	do {
-		modulo = numN % 10;
-		numN = numN / 10;
-		a[numdigit] = modulo;
-		numdigit++;
-	} while (numN > 0);
+	numdigit = numDigit(n);
+	reverNum = numRever(numdigit - 1, n);
 
-	for (i = numdigit - 1; i >= 0; i--)
+	while (reverNum > 0)
 	{
-		_putchar('0' + a[i]);
+		lastDigit = reverNum % 10;
+		reverNum = reverNum / 10;
+		_putchar('0' + lastDigit);
 	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else if (n % 10 == 0)
+	{
+		for (i = 0; i < numdigit - numDigit(numRever(numdigit - 1, n)); i++)
+		{
+			_putchar('0');
+		}
+	}
+}
+/**
+ * numDigit - counter digits
+ * @number: number
+ *
+ * Return: is a void
+ */
+int numDigit(int number)
+{
+	int counter = 0;
+
+	while (number > 0)
+	{
+		number = number / 10;
+		counter++;
+	}
+
+	return (counter);
+}
+/**
+ * numRever - reverse number
+ * @numbDigit: digits
+ * @Number: number
+ *
+ * Return: is a void
+ */
+int numRever(int numbDigit, int Number)
+{
+	int lastDigit, reverNum = 0;
+
+	while (Number > 0)
+	{
+		lastDigit = Number % 10;
+		Number = Number / 10;
+
+		reverNum += lastDigit * expo(numbDigit);
+		numbDigit--;
+	}
+
+	return (reverNum);
+}
+/**
+ * expo - exponential
+ * @counter: exponential
+ *
+ * Return: is a void
+ */
+int expo(int counter)
+{
+	int i, expoNum = 10;
+
+	if (counter == 0)
+	{
+		return (1);
+	}
+
+	for (i = 1; i < counter; i++)
+	{
+		expoNum *= 10;
+	}
+
+	return (expoNum);
 }
