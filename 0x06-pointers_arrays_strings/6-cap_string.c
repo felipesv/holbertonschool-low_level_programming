06c66e7ac2932a58f2a6afb333d2467b76afb098
Felipe Satizabal <1165@holbertonschool.com>
2019-10-03 19:56:34 +0000

    Fix file 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
 /**
  * cap_string - capitalizes all words of a string.
- * @dest: string 1
- * @src: string 2
+ * @s: array
  *
  * Return: return string cocatenated
  */
@@ -10,13 +9,13 @@ char *cap_string(char *s)
 {
 	int length = 0;
 
-	while(s[length] != '\0')
+	while (s[length] != '\0')
 	{
-		if(specialCharact(s[length]) && !specialCharact(s[length + 1]))
+		if (specialCharact(s[length]) && !specialCharact(s[length + 1]))
 		{
 			length++;
 
-			if(s[length] >= 'a' && s[length + 1] <= 'z')
+			if (s[length] >= 'a' && s[length + 1] <= 'z')
 			{
 				s[length] = s[length] - 32;
 			}
@@ -27,7 +26,12 @@ char *cap_string(char *s)
 
 	return (s);
 }
-
+/**
+ * specialCharact - especial character
+ * @a: char
+ *
+ * Return: 0 no especial, 1 especial
+ */
 int specialCharact(char a)
 {
 	switch (a)
@@ -45,9 +49,8 @@ int specialCharact(char a)
 	case ' ':
 	case '\n':
 	case '\t':
-		return 1;
-		break;
+		return (1);
 	}
 
-	return 0;
+	return (0);
 }
