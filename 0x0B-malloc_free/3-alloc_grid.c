5a5befde9c14544ce010f63e3332082429a23163
Felipe Satizabal <1165@holbertonschool.com>
2019-10-15 20:04:02 +0000

    Fix 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	p = (int **) malloc(height);
+	p = (int **) malloc(height * sizeof(int *));
 
 	if (p == NULL)
 		return (NULL);
