67c20c8b95d3872d4834adee59de739999869349
Felipe Satizabal <1165@holbertonschool.com>
2019-10-15 22:27:01 +0000

    Add file 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,62 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * argstostr -  concatenates all the arguments
+ * @ac: no arguments
+ * @av: arguments
+ *
+ * Return: pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	char *p, ln = 0, cnt = 0, i = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	while (*(av + cnt))
+	{
+		ln += fn_lengthOfString(*(av + cnt));
+		cnt++;
+	}
+
+	p = malloc(ln + ac + 1);
+	cnt = 0;
+
+	while (*av)
+	{
+		char *c = *av;
+
+		cnt = 0;
+		ln += fn_lengthOfString(c);
+
+		while (*(c + cnt))
+		{
+			*(p + (i++)) = *c;
+			c++;
+		}
+
+		*(p + (i++)) = '\n';
+		av++;
+	}
+
+	return (p);
+}
+/**
+ * fn_lengthOfString - length of a string.
+ * @p: string
+ *
+ * Return: length
+ */
+int fn_lengthOfString(char *p)
+{
+	int length = 0;
+
+	while (*(p +  length))
+	{
+		length++;
+	}
+
+	return (length);
+}
