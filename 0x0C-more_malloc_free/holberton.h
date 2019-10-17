5773e94d032e25c32ef08dd583faa31caf89a004
Felipe Satizabal <1165@holbertonschool.com>
2019-10-17 19:20:10 +0000

    Fix 2

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,11 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int fn_lengthOfString(char *p);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *_memset(char *s, char b, unsigned int n);
-
 
 #endif /* HOLBERTON_H */
