09082a9f7bd98b1d5c98418de208ae60a3915c62
Felipe Satizabal <1165@holbertonschool.com>
2019-10-21 20:48:44 +0000

    Fix 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,15 +11,11 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int ln_name, ln_owner;
+	int ln_name, ln_owner, cnt1 = 0, cnt2 = 0;
 	dog_t *nw_dog;
-
-	if (name == NULL || owner == NULL)
-		return (NULL);
 
 	ln_name = lengthArray(name);
 	ln_owner = lengthArray(owner);
-
 	nw_dog = malloc(sizeof(dog_t));
 
 	if (nw_dog == NULL)
@@ -33,7 +29,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	while (cnt1 <= ln_name)
+	{
+		(*nw_dog).name[cnt1] = name[cnt1];
+		cnt1++;
+	}
+
 	(*nw_dog).owner = malloc(sizeof(char) * ln_owner);
+	(*nw_dog).age = age;
 
 	if ((*nw_dog).owner == NULL)
 	{
@@ -42,12 +45,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	(*nw_dog).name = name;
-	(*nw_dog).age = age;
-	(*nw_dog).owner = owner;
-
+	while (cnt2 <= ln_name)
+	{
+		(*nw_dog).owner[cnt2] = name[cnt2];
+		cnt2++;
+	}
 	return (nw_dog);
-
 }
 /**
  * lengthArray - length array
