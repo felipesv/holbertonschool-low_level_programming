18ee4c8538bf8628daecd2a022a39c17d4a5a398
Felipe Satizabal <1165@holbertonschool.com>
2019-10-24 00:00:11 +0000

    Fix 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <string.h>
 /**
  * main - print result of basic operations
  * @argc: number of command line arguments
@@ -16,9 +17,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (!(argv[2][0] == '+' || argv[2][0] == '-' ||
-	      argv[2][0] == '/' || argv[2][0] == '*' ||
-	      argv[2][0] == '%'))
+	if (get_op_func(argv[2]) == NULL || strlen(argv[2]) != 1)
 	{
 		printf("Error\n");
 		exit(99);
