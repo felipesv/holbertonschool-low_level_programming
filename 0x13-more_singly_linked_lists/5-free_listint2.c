03d46bb9838ece6441c79a5e05d92780020249e2
Felipe Satizabal <1165@holbertonschool.com>
2019-11-05 14:18:07 +0000

    Fix 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current = *head;
+	listint_t *next;
 
 	if (current != NULL)
 	{
 		while (current != NULL)
 		{
+			next = current->next;
 			free(current);
-			current = current->next;
+			current = next;
 		}
 		*head = NULL;
 	}
