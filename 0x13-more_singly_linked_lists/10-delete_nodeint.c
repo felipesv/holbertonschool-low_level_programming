2f3dc4809a1243f5b554414bb6d9770482cda562
Felipe Satizabal <1165@holbertonschool.com>
2019-11-05 22:39:23 +0000

    Fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (index == 0)
 		{
 			current = *head;
-			n = current->n;
+			n = 1;
 			*head = current->next;
 			free(current);
 			return (n);
