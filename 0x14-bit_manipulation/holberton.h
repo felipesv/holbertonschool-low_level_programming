e4dcb1654cef52b6258483497d4a591dd9fc9e1e
Felipe Satizabal <1165@holbertonschool.com>
2019-11-07 21:20:05 +0000

    Add 2

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -13,5 +13,6 @@ int _pow_recursion(int x, int y);
 void print_binary(unsigned long int n);
 void _print_binary(unsigned long int n);
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif /* HOLBERTON_H */
