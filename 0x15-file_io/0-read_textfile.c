b67fcf39613b0e808e7a7890b790d6ea30200fe6
Felipe Satizabal <1165@holbertonschool.com>
2019-11-12 19:26:13 +0000

    Fix 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -31,7 +31,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (rd < 0)
 		return (0);
 
-	wr = write(STDOUT_FILENO, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
 
 	if (wr < 0)
 		return (0);
