dee8704f4a02dfff1551b7b270a5a2fc4c091e48
felipesv <1165@holbertonschool.com>
2020-01-17 15:39:01 -0500

    fix betty

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,15 +2,17 @@
 
 /**
  * key_index - create a key to store in hash table
+ * @key: key val in the hash
+ * @size: size of the hash table
  *
  * Return: key in the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        unsigned long int key_hash = 0;
-        unsigned long int dbj2 = 0;
+	unsigned long int key_hash = 0;
+	unsigned long int dbj2 = 0;
 
-        dbj2 = hash_djb2(key);
-        key_hash = dbj2 % size;
-        return (key_hash);
+	dbj2 = hash_djb2(key);
+	key_hash = dbj2 % size;
+	return (key_hash);
 }
