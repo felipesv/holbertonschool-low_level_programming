b99d795d23fb7a40919eb672a56438b325f99697
felipesv <1165@holbertonschool.com>
2020-01-19 14:30:49 -0500

    fix

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,7 +9,7 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int cnt = 0;
-	hash_node_t *node = NULL;
+	hash_node_t *node = NULL, *temp = NULL;
 
 	if (ht == NULL)
 		return;
@@ -19,26 +19,16 @@ void hash_table_delete(hash_table_t *ht)
 		for (cnt = 0; cnt < (*ht).size; cnt++)
 		{
 			node = (*ht).array[cnt];
-			free_list(node);
+			while (node != NULL)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
 		}
 		free((*ht).array);
 	}
 	free(ht);
-}
-/**
- * free_list - free a list
- * @head: list
- *
- * Return: pointer
- */
-void free_list(hash_node_t *head)
-{
-
-	if (head != NULL)
-	{
-		free_list(head->next);
-		free(head->key);
-		free(head->value);
-		free(head);
-	}
 }
