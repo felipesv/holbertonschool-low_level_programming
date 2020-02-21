5ccd94de28bf957bcc4503e418f1bfbacdcf79be
Felipe Satizabal <1165@holbertonschool.com>
2020-02-21 15:37:19 -0500

    fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,9 +9,9 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 1:
+                if i > 0 and grid[i - 1][j] == 1:
                     count2 += 1
-                if grid[i][j - 1] == 1:
+                if j > 0 and grid[i][j - 1] == 1:
                     count2 += 1
                 count1 += 1
     val = (count1 * 4) - (count2 * 2)
