fdff003999dbdd2f24385ad9448e4925ef515046
Felipe Satizabal <1165@holbertonschool.com>
2020-02-21 15:32:24 -0500

    fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,8 +6,8 @@ def island_perimeter(grid):
     """returns the perimeter of the island"""
     count1 = 0
     count2 = 0
-    for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[i]) - 1):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 if grid[i - 1][j] == 1:
                     count2 += 1
