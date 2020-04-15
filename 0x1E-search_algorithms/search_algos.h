0991ccd26e20016e07db296b415109454b7bdf6a
felipesv <1165@holbertonschool.com>
2020-04-15 12:38:33 -0500

    add task 1 and .h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_recursion(int *array, int value, size_t from, size_t to);
+void print_array(int *array, size_t from, size_t to);
+
+
+#endif /* SEARCH_ALGOS_H */
